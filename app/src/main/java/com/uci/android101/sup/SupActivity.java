ba46adf0cc620af7d37cb2b3d8d2202658d01917
somarkoe <somarkoe@gmail.com>
2016-11-01 18:48:52 -0700

    Initial commit.

---
+++ app/src/main/java/com/uci/android101/sup/SupActivity.java
@@ -0,0 +1,13 @@
+package com.uci.android101.sup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sup);
+    }
+}

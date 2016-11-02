f6ceb505245cc50eca5dd22aa0fb248226160dc9
somarkoe <somarkoe@gmail.com>
2016-11-02 12:21:46 -0700

    Sup implementation

---
+++ app/src/main/java/com/uci/android101/sup/Friend.java
@@ -0,0 +1,27 @@
+package com.uci.android101.sup;
+
+/**
+ * Created by somarkoe on 11/2/16.
+ */
+
+public class Friend {
+
+    private String mFriendName;
+    private String mPhoneNumber;
+
+    public String getFriendName() {
+        return mFriendName;
+    }
+
+    public void setFriendName(String friendName) {
+        this.mFriendName = friendName;
+    }
+
+    public String getPhoneNumber() {
+        return mPhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.mPhoneNumber = phoneNumber;
+    }
+}

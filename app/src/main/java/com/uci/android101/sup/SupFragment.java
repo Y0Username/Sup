495a7eefc6e162e650132f9edd223a5767ea65df
Markoe <markoe@amazon.com>
2016-11-07 15:55:03 -0800

    Resetting friend after sup sent

--- app/src/main/java/com/uci/android101/sup/SupFragment.java
+++ app/src/main/java/com/uci/android101/sup/SupFragment.java
@@ -178,6 +178,7 @@ public class SupFragment extends Fragment {
             e.printStackTrace();
         } finally {
             mSelectFriend.setText(R.string.select_friend);
+            mFriend = new Friend();
             saveSup();
             setLatestSup();
         }
